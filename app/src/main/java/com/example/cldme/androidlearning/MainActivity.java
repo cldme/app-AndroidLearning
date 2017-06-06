c4c9d3d2e7a7ec478ebb7d77d0fd735e4b1d98a0
Claudiu Ion <ioncld97@gmail.com>
2017-06-06 21:25:50 +0200

    Yes/No App functionality has been added together with user input names

--- app/src/main/java/com/example/cldme/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/cldme/androidlearning/MainActivity.java
@@ -4,25 +4,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button answerYesButton, answerNoButton;
+    private EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView firstTextView = (TextView) findViewById(R.id.textView);
+        answerYesButton = (Button) findViewById(R.id.answerYes);
 
-        Button firstButton = (Button) findViewById(R.id.button1);
+        answerNoButton = (Button) findViewById(R.id.answerNo);
 
-        firstButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        userName = (EditText) findViewById(R.id.userName);
+    }
 
-                firstTextView.setText("You Clicked!");
-            }
-        });
+    public void onYesButtonClick(View view) {
+
+        String userNameText = String.valueOf(userName.getText());
+
+        String response = "That is great " + userNameText;
+
+        Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+
+    }
+
+    public void onNoButtonClick(View view) {
+
+        String userNameText = String.valueOf(userName.getText());
+
+        String response = "To bad, unlucky, " + userNameText;
+
+        Toast.makeText(this, response, Toast.LENGTH_LONG).show();
+
     }
 }
